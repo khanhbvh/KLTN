87da7d5adc49942a2d08b7740c006f02b77b4157
khanhbvh <khanhbvh93@gmail.com>
2015-11-29 09:42:12 +0700

    update database - create table country for video

--- docs/database/sharingvideo.sql
+++ docs/database/sharingvideo.sql
@@ -23,6 +23,7 @@ CREATE TABLE IF NOT EXISTS `sharingvideodb`.`category` (
   `CategoryDes` LONGTEXT CHARACTER SET 'utf8' COLLATE 'utf8_unicode_ci' NULL DEFAULT NULL COMMENT '',
   PRIMARY KEY (`CategoryId`)  COMMENT '')
 ENGINE = InnoDB
+AUTO_INCREMENT = 5
 DEFAULT CHARACTER SET = utf8;
 
 
@@ -42,17 +43,18 @@ CREATE TABLE IF NOT EXISTS `sharingvideodb`.`user` (
   `Avatar` VARCHAR(100) NULL DEFAULT NULL COMMENT '',
   PRIMARY KEY (`UserId`)  COMMENT '')
 ENGINE = InnoDB
+AUTO_INCREMENT = 3
 DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
--- Table `sharingvideodb`.`Type`
+-- Table `sharingvideodb`.`type`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `sharingvideodb`.`Type` (
-  `TypeId` INT NOT NULL AUTO_INCREMENT COMMENT '',
+CREATE TABLE IF NOT EXISTS `sharingvideodb`.`type` (
+  `TypeId` INT(11) NOT NULL AUTO_INCREMENT COMMENT '',
   `CategoryId` INT(11) NOT NULL COMMENT '',
   `TypeName` VARCHAR(100) CHARACTER SET 'utf8' COLLATE 'utf8_unicode_ci' NOT NULL COMMENT '',
-  `TypeDes` LONGTEXT CHARACTER SET 'utf8' COLLATE 'utf8_unicode_ci' NULL COMMENT '',
+  `TypeDes` LONGTEXT CHARACTER SET 'utf8' COLLATE 'utf8_unicode_ci' NULL DEFAULT NULL COMMENT '',
   PRIMARY KEY (`TypeId`, `CategoryId`)  COMMENT '',
   INDEX `fk_Type_category1_idx` (`CategoryId` ASC)  COMMENT '',
   CONSTRAINT `fk_Type_category1`
@@ -60,6 +62,18 @@ CREATE TABLE IF NOT EXISTS `sharingvideodb`.`Type` (
     REFERENCES `sharingvideodb`.`category` (`CategoryId`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
+ENGINE = InnoDB
+AUTO_INCREMENT = 14
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `sharingvideodb`.`Country`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `sharingvideodb`.`Country` (
+  `CountryId` INT NOT NULL AUTO_INCREMENT COMMENT '',
+  `CountryName` VARCHAR(100) CHARACTER SET 'utf8' COLLATE 'utf8_unicode_ci' NOT NULL COMMENT '',
+  PRIMARY KEY (`CountryId`)  COMMENT '')
 ENGINE = InnoDB;
 
 
@@ -69,31 +83,39 @@ ENGINE = InnoDB;
 CREATE TABLE IF NOT EXISTS `sharingvideodb`.`video` (
   `VideoId` INT(11) NOT NULL AUTO_INCREMENT COMMENT '',
   `UserId` INT(11) NOT NULL COMMENT '',
-  `TypeId` INT NOT NULL COMMENT '',
+  `TypeId` INT(11) NOT NULL COMMENT '',
   `CategoryId` INT(11) NOT NULL COMMENT '',
+  `CountryId` INT NOT NULL COMMENT '',
   `Title` VARCHAR(200) CHARACTER SET 'utf8' COLLATE 'utf8_unicode_ci' NOT NULL COMMENT '',
   `FilePath` VARCHAR(200) CHARACTER SET 'utf8' COLLATE 'utf8_unicode_ci' NOT NULL COMMENT '',
   `Size` MEDIUMINT(9) NULL DEFAULT NULL COMMENT '',
-  `Description` LONGTEXT CHARACTER SET 'utf8' COLLATE 'utf8_unicode_ci' NULL COMMENT '',
+  `Description` LONGTEXT CHARACTER SET 'utf8' COLLATE 'utf8_unicode_ci' NULL DEFAULT NULL COMMENT '',
   `DateCreate` DATETIME NOT NULL COMMENT '',
-  `NumView` INT NULL COMMENT '',
+  `NumView` INT(11) NULL DEFAULT NULL COMMENT '',
   `NumLike` INT(11) NULL DEFAULT NULL COMMENT '',
-  `NumShare` INT(11) NULL DEFAULT NULL COMMENT '',
-  `Img` VARCHAR(100) CHARACTER SET 'utf8' COLLATE 'utf8_unicode_ci' NULL DEFAULT NULL COMMENT '',
-  PRIMARY KEY (`VideoId`, `UserId`, `TypeId`, `CategoryId`)  COMMENT '',
+  `NumShare` INT(11) NULL COMMENT '',
+  `Img` VARCHAR(100) CHARACTER SET 'utf8' COLLATE 'utf8_unicode_ci' NULL COMMENT '',
+  PRIMARY KEY (`VideoId`, `UserId`, `TypeId`, `CategoryId`, `CountryId`)  COMMENT '',
   INDEX `fk_video_user1_idx` (`UserId` ASC)  COMMENT '',
   INDEX `fk_video_Type1_idx` (`TypeId` ASC, `CategoryId` ASC)  COMMENT '',
+  INDEX `fk_video_Country2_idx` (`CountryId` ASC)  COMMENT '',
+  CONSTRAINT `fk_video_Type1`
+    FOREIGN KEY (`TypeId` , `CategoryId`)
+    REFERENCES `sharingvideodb`.`type` (`TypeId` , `CategoryId`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
   CONSTRAINT `fk_video_user1`
     FOREIGN KEY (`UserId`)
     REFERENCES `sharingvideodb`.`user` (`UserId`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_video_Type1`
-    FOREIGN KEY (`TypeId` , `CategoryId`)
-    REFERENCES `sharingvideodb`.`Type` (`TypeId` , `CategoryId`)
+  CONSTRAINT `fk_video_Country2`
+    FOREIGN KEY (`CountryId`)
+    REFERENCES `sharingvideodb`.`Country` (`CountryId`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
+AUTO_INCREMENT = 39
 DEFAULT CHARACTER SET = utf8;
 
 
@@ -109,14 +131,14 @@ CREATE TABLE IF NOT EXISTS `sharingvideodb`.`comment` (
   PRIMARY KEY (`CommentId`, `VideoId`, `UserId`)  COMMENT '',
   INDEX `fk_comment_video1_idx` (`VideoId` ASC)  COMMENT '',
   INDEX `fk_comment_user1_idx` (`UserId` ASC)  COMMENT '',
-  CONSTRAINT `fk_comment_video1`
-    FOREIGN KEY (`VideoId`)
-    REFERENCES `sharingvideodb`.`video` (`VideoId`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
   CONSTRAINT `fk_comment_user1`
     FOREIGN KEY (`UserId`)
     REFERENCES `sharingvideodb`.`user` (`UserId`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_comment_video1`
+    FOREIGN KEY (`VideoId`)
+    REFERENCES `sharingvideodb`.`video` (`VideoId`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
@@ -130,7 +152,7 @@ COLLATE = utf8_unicode_ci;
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `sharingvideodb`.`recentlyviewed` (
   `UserId` INT(11) NOT NULL COMMENT '',
-  `Viewed_date` DATETIME NOT NULL COMMENT '',
+  `Viewed_date` DATE NOT NULL COMMENT '',
   `VideoId` INT(11) NOT NULL COMMENT '',
   PRIMARY KEY (`UserId`, `VideoId`)  COMMENT '',
   INDEX `fk_RECENTLYVIEWED_Video1_idx` (`VideoId` ASC)  COMMENT '',
